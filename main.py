41a23186dba660589ef71b5f356f0846bae1eb26
Nohelynaid <nohelynaid@gmail.com>
2022-03-13 18:58:45 -0600

    cambios diana

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/diana/prueba")
 def hello():
     return "Hello, World!"
 
+
+@app.route("/diana/prueba2")
+def hello1():
+    
+    return "Hello, putos!"
 
 app.run()
