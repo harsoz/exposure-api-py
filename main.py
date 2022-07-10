954444b6d115e7abf1eb8431fbf8db6e5ad9d75b
harsoz <66538679+harsoz@users.noreply.github.com>
2022-07-09 19:01:30 -0500

    update cors

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask
-from flask_cors import cross_origin
+from flask_cors import CORS
 from controllers.home_controller import home
 from controllers.bfs_controller import bfs
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 app.register_blueprint(home, url_prefix='/api/v1/home')
 app.register_blueprint(bfs, url_prefix='/api/v1/bfs')
 
 @app.route('/')
-@cross_origin()
 def index():
     return "Hello World!"
 
